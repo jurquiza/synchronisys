09b84234590e47dd297d073a5258e055fb7ddb0f
jurquiza <uriel.urquiza@gmail.com>
2016-09-03 07:21:24 +0100

    building reports view and template

--- sloppycellreports/views.py
+++ sloppycellreports/views.py
@@ -3,7 +3,10 @@ import os
 # Create your views here.
 
 def sloppycell_reports_front(request):
-	return render(request, 'sloppycellreports/scr_front.html',{})
+    dir_list = os.listdir('static/')
+    return render(request, 'sloppycellreports/scr_front.html',{'dir_list' : dir_list})
 
 def sloppycell_reports_display_current_fitting(request):
-	return render(request, 'sloppycellreports/scr_current_fitting.html',{})
+    dir_list = os.listdir('static/')
+    cost = os.listdir('static/mock1')
+    return render(request, 'sloppycellreports/scr_current_fitting.html',{'constraints': dir_list, 'cost' : cost})
