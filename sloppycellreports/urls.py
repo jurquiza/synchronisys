8ffd0a4e4e2a4fccac2fde1f2aa96b1adb7f8860
jurquiza <uriel.urquiza@gmail.com>
2016-09-03 12:15:05 +0100

    Now it displays the current fitting and previous ones

--- sloppycellreports/urls.py
+++ sloppycellreports/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import  include,url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.sloppycell_reports_front, name='sloppycell_reports_front'),
-	url(r'scr_current_fitting/$', views.sloppycell_reports_display_current_fitting, name= "sloppycell_reports_display_current_fitting")
+        url(r'^$', views.sloppycell_reports_front),
+        url(r'scr_current_fitting/$', views.sloppycell_reports_display_current_fitting),
+        url(r'scr_current_fitting/(?P<fitting_folder>[0-9]{6})$', views.sloppycell_reports_display_current_fitting),
 ]
--- sloppycellreports/views.py
+++ sloppycellreports/views.py
@@ -6,7 +6,12 @@ def sloppycell_reports_front(request):
     dir_list = os.listdir('static/')
     return render(request, 'sloppycellreports/scr_front.html',{'dir_list' : dir_list})
 
-def sloppycell_reports_display_current_fitting(request):
-    dir_list = os.listdir('static/')
-    cost = os.listdir('static/mock1')
-    return render(request, 'sloppycellreports/scr_current_fitting.html',{'constraints': dir_list, 'cost' : cost})
+def sloppycell_reports_display_current_fitting(request,fitting_folder="current_fitting"):
+    dir_list = os.listdir('sloppycellreports/static/sloppycellreports/'+fitting_folder+'/images/')
+    constraints =[]
+    for file_item in dir_list:
+        if 'constraint' in file_item:
+            constraints.append(file_item)
+        elif 'cost' in file_item:
+            cost = file_item 
+    return render(request, 'sloppycellreports/scr_current_fitting.html',{'fitting_folder': fitting_folder,'constraints': constraints, 'cost': cost})
