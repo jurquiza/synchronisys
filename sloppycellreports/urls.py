28fe4ad2b0f9a61e143d75673ed76b41dc2b98b8
jurquiza <uriel.urquiza@gmail.com>
2016-09-05 17:58:38 +0100

    No it is possible to visualise the fitting where the networks are diplayed indpendent of each other. Also I have to add visualutation of constrains. I need to write a more elegant way for showing the differente pages

--- sloppycellreports/urls.py
+++ sloppycellreports/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
         url(r'^$', views.sloppycell_reports_front),
         url(r'scr_current_fitting/$', views.sloppycell_reports_display_current_fitting),
-        url(r'scr_current_fitting/(?P<fitting_folder>[0-9]{6})$', views.sloppycell_reports_display_current_fitting),
+        url(r'scr_current_fitting/(?P<fitting_folder>[0-9]{6})_sloppyCell$', views.sloppycell_reports_display_current_fitting),
+        url(r'scr_current_fitting/(?P<fitting_folder>[0-9]{6})_sloppyCell/network/(?P<network>.*)$', views.sloppycell_reports_display_current_fitting_network),
+        url(r'notebook/(?P<notebook_folder>[0-9]{6})$', views.sloppycell_reports_display_notebook),
 ]
--- sloppycellreports/views.py
+++ sloppycellreports/views.py
@@ -2,16 +2,48 @@ from django.shortcuts import render
 import os
 # Create your views here.
 
+## This fucntion gets the directories name and and resturns a filtered list
+def listdir_except(path, avoid):
+    for f in os.listdir(path):
+        for l in avoid:
+            if l in f:
+                break
+            else:
+               yield f
+               break
+
+
 def sloppycell_reports_front(request):
-    dir_list = os.listdir('static/')
+    dir_list = listdir_except('sloppycellreports/static/sloppycellreports',avoid=['.DS','populate'])
     return render(request, 'sloppycellreports/scr_front.html',{'dir_list' : dir_list})
 
 def sloppycell_reports_display_current_fitting(request,fitting_folder="current_fitting"):
-    dir_list = os.listdir('sloppycellreports/static/sloppycellreports/'+fitting_folder+'/images/')
+    networks = listdir_except('sloppycellreports/static/sloppycellreports/'+fitting_folder+'_sloppyCell/images/',avoid=['.png'])
+    dir_list = os.listdir('sloppycellreports/static/sloppycellreports/'+fitting_folder+'_sloppyCell/images/')
     constraints =[]
     for file_item in dir_list:
         if 'constraint' in file_item:
             constraints.append(file_item)
         elif 'cost' in file_item:
             cost = file_item 
-    return render(request, 'sloppycellreports/scr_current_fitting.html',{'fitting_folder': fitting_folder,'constraints': constraints, 'cost': cost})
+    return render(request, 'sloppycellreports/scr_current_fitting.html',{'networks':networks,'fitting_folder': fitting_folder,'constraints': constraints, 'cost': cost})
+
+
+def sloppycell_reports_display_current_fitting_network(request,fitting_folder="current_fitting",network=0):
+    networks = listdir_except('sloppycellreports/static/sloppycellreports/'+fitting_folder+'_sloppyCell/images/',avoid=['.png'])
+    network_plots = os.listdir('sloppycellreports/static/sloppycellreports/'+fitting_folder+'_sloppyCell/images/'+network)
+    dir_list = os.listdir('sloppycellreports/static/sloppycellreports/'+fitting_folder+'_sloppyCell/images/')
+    constraints =[]
+    for file_item in dir_list:
+        if 'constraint' in file_item:
+            constraints.append(file_item)
+        elif 'cost' in file_item:
+            cost = file_item 
+    return render(request, 'sloppycellreports/scr_network.html',{'networks':networks,'network':network , 'network_plots': network_plots,
+            'fitting_folder': fitting_folder,'constraints': constraints, 'cost': cost})
+
+
+
+def sloppycell_reports_display_notebook(request,notebook_folder="020916"):
+
+    return render(request, 'sloppycellreports/notebook/'+notebook_folder+'_sloppyCell/html/fitting_'+notebook_folder+'.html')
